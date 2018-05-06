815afe1776164a5bc6c4c26e60d3bbc57e2e060a
fangzheng.xu <fangzheng.xu@ele.me>
2018-05-06 23:42:38 +0800

    double the rest request timeout time

--- src/main/java/me/fetonxu/tank_console/RestTemplateConfig.java
+++ src/main/java/me/fetonxu/tank_console/RestTemplateConfig.java
@@ -16,8 +16,8 @@ public class RestTemplateConfig {
     @Bean
     public ClientHttpRequestFactory simpleClientHttpRequestFactory(){
         SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
-        factory.setReadTimeout(5000);//单位为ms
-        factory.setConnectTimeout(5000);//单位为ms
+        factory.setReadTimeout(10000);//单位为ms
+        factory.setConnectTimeout(10000);//单位为ms
         return factory;
     }
 }
